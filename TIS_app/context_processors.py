4b3ec053d4f38c71d546d548c2cd8b48aad054ea
Andrzej Jończy <jonek.andree@gmail.com>
2022-05-22 18:02:42 +0200

    Revert "copy from cs50 branch, my final project"
    This reverts commit fe2c771ba8bc3682771b9ccd15e5c6c685ff1435.

--- TIS_app/context_processors.py
+++
@@ -1,6 +0,0 @@
-def metadata(request):
-    return {
-        "author": "Andrzej Jończy",
-        "github": "https://github.com/andree0",
-        "linkedin": "https://www.linkedin.com/in/andrzej-jonczy/",
-    }
